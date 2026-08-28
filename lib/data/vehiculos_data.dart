7fac4edb8de0cbefcfeef90de1d818a99b82e150
Freddy Andres Moran Soriano <freddy.moran.soriano@gmail.com>
2026-08-27 20:59:08 -0500

    Creación de los datos del catálogo

---
+++ lib/data/vehiculos_data.dart
@@ -0,0 +1,56 @@
+import '../models/vehiculo.dart';
+
+const List<Vehiculo> vehiculos = [
+  Vehiculo(
+    id: 1,
+    nombre: 'Toyota Supra',
+    tipo: 'Auto deportivo',
+    motor: '3.0 L, 6 cilindros Twin-Scroll Turbo',
+    potencia: '382 HP',
+    transmision: 'Automática de 8 velocidades',
+    traccion: 'Trasera',
+    combustible: 'Gasolina',
+    imagenUrl:
+        'https://images.unsplash.com/photo-1583121274602-3e2820c69888',
+    sitioWeb: 'https://www.toyota.com/grsupra/',
+  ),
+  Vehiculo(
+    id: 2,
+    nombre: 'Ford Mustang',
+    tipo: 'Muscle car',
+    motor: '5.0 L V8',
+    potencia: '480 HP',
+    transmision: 'Manual de 6 velocidades',
+    traccion: 'Trasera',
+    combustible: 'Gasolina',
+    imagenUrl:
+        'https://images.unsplash.com/photo-1584345604476-8ec5e12e42dd',
+    sitioWeb: 'https://www.ford.com/cars/mustang/',
+  ),
+  Vehiculo(
+    id: 3,
+    nombre: 'Nissan GT-R',
+    tipo: 'Deportivo japonés',
+    motor: '3.8 L V6 Twin Turbo',
+    potencia: '565 HP',
+    transmision: 'Automática de doble embrague, 6 velocidades',
+    traccion: 'AWD',
+    combustible: 'Gasolina',
+    imagenUrl:
+        'https://images.unsplash.com/photo-1606664515524-ed2f786a0bd6',
+    sitioWeb: 'https://www.nissanusa.com/vehicles/sports-cars/gt-r.html',
+  ),
+  Vehiculo(
+    id: 4,
+    nombre: 'Chevrolet Corvette',
+    tipo: 'Deportivo americano',
+    motor: '6.2 L V8',
+    potencia: '495 HP',
+    transmision: 'Automática de doble embrague, 8 velocidades',
+    traccion: 'Trasera',
+    combustible: 'Gasolina',
+    imagenUrl:
+        'https://images.unsplash.com/photo-1553440569-bcc63803a83d',
+    sitioWeb: 'https://www.chevrolet.com/performance/corvette',
+  ),
+];
