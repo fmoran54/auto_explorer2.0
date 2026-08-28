bc00fb7aec5ff2133db8ef3af0e9dab7dd4808f9
Freddy Andres Moran Soriano <freddy.moran.soriano@gmail.com>
2026-08-27 20:49:54 -0500

    Creación del modelo de vehículos

---
+++ lib/models/vehiculo.dart
@@ -0,0 +1,25 @@
+class Vehiculo {
+  final int id;
+  final String nombre;
+  final String tipo;
+  final String motor;
+  final String potencia;
+  final String transmision;
+  final String traccion;
+  final String combustible;
+  final String imagenUrl;
+  final String sitioWeb;
+
+  const Vehiculo({
+    required this.id,
+    required this.nombre,
+    required this.tipo,
+    required this.motor,
+    required this.potencia,
+    required this.transmision,
+    required this.traccion,
+    required this.combustible,
+    required this.imagenUrl,
+    required this.sitioWeb,
+  });
+}
